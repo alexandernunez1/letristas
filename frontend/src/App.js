bcd4762c5ec62ee7428e1a9adf01061a25654c89
Alex Nunez <nunezaleks@gmail.com>
2021-11-21 22:04:12 -0800

    Merge branch 'main' of https://github.com/alexandernunez1/letristas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+
+import Home from './pages/home/Home'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes 
+} from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
-          social media for letter enthusiasts
+         <Home />
     </div>
   );
 }

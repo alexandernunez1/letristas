4dc01c3f9da044a38dcd3509d149bf17c0be8637
Alex Nunez <nunezaleks@gmail.com>
2021-11-24 21:03:55 -0800

    import and return TopBar component

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Topbar from '../../components/topbar/Topbar';
 
 const Home = () => {
     return (
         <div>
+            <Topbar />
             welcome to the home page
         </div>
     )

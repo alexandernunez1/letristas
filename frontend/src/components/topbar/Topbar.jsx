2ad17dd598031c27c60d1cc6aa93ba283741ad28
Alex Nunez <nunezaleks@gmail.com>
2022-02-23 00:45:30 -0800

    update topbar structure and style

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -2,32 +2,40 @@ import React from "react";
 import "./topbar.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import PersonIcon from "@mui/icons-material/Person";
+import HomeIcon from '@mui/icons-material/Home';
 
-const Topbar = () => {
+
+export default function Topbar() {
   return (
     <div className="topbarContainer">
+            <img src="/assets/Tech_Mariposa.png" alt="butterfly" className="topbarImg"/>
       <div className="topbarLeft">
-        <span className="logo">letristas</span>
-      </div>
-      <div className="topbarRight">
-        <div className="topbarLinks">
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
-        </div>
+        <span className="logo">
+          Letristas</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-          <SearchRoundedIcon />
-          <input placeholder="search" type="text" className="searchInput" />
+          <SearchRoundedIcon className="searchIcon" />
+          <input
+            placeholder="Search"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          
+          <span className="topbarLink">Feed</span>
+          <span className="topbarLink">Photos</span>
         </div>
         <div className="topbarIcons">
-          <div className="topbarIconItem">
+        <div className="homeIcon"><HomeIcon /></div>
+          <div className="personIcon">
             <PersonIcon />
           </div>
+
         </div>
       </div>
     </div>
   );
-};
-
-export default Topbar;
+}
